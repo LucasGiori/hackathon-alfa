2ef9b1e2df59c85ab9fc5b9092e8d5206e6390b0
Lucas Giori Cesconetto <lucasgioricesconetto@gmail.com>
2021-06-17 01:35:20 -0300

    f

--- admin-laravel/resources/views/layouts/app.blade.php
+++ admin-laravel/resources/views/layouts/app.blade.php
@@ -68,10 +68,6 @@
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                            onclick="event.preventDefault();
-                                           document.cookie = 'XSRF-TOKEN=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-                                           document.cookie = 'laravel_session=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-                                           window.location.reload();
-
                                         ">
                                             {{ __('Sair') }}
                                         </a>
